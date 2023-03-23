5db02e5e919149b86d2a5cd8f00572ba91ffe476
Samuel Motas <smlmotas@gmail.com>
2023-03-23 18:56:00 -0400

    projet pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+interface Pokemons {
+  name: string
+  url: string
+}
+
 function App() {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+
+    const getPokemon = async () => {
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
+        )
+        console.log(res.data.results)
+
+        res.data.results.forEach(async (pokemon: Pokemons) => {
+          const poke = await axios.get("https://pokeapi.co/api/v2/pokemon/${pokemon.name}")
+        })
+        
+    }
+    
+    getPokemon()
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="pokemon-header">
+        Pokemon
       </header>
     </div>
   );
