270dc3de8ca698cba61846970bac034188b5deb2
Samuel Motas <smlmotas@gmail.com>
2023-03-26 17:05:15 -0400

    set

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -11,7 +11,7 @@ function PokemonList(props: Props) {
     const { name, id, image, type } = props
     return <div>
         <section className={`pokemon-list-container ${type}`}>
-            <p className='pokemon-name'># {id}</p>
+            <p className='pokemon-name'>#{id}</p>
             <p className='pokemon-name'>  {name}</p>
             <img src={image} alt={name} />
             <p className='pokemon-name'>Type : {type}</p>
